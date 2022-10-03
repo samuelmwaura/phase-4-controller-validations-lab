d6f5d9659e2e995b63259a5d544fd8438845be1f
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-03 11:58:46 +0300

    Completed the lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]} #Checks that the category is either of the the two elements in the array
+    validates :content, length: {minimum: 100}
 end
